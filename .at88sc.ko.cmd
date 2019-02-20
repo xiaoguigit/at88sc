d983f314e8eb19570630440bcd928f324ca9a02a
mr_xiaogui <mr_xiaogui@163.com>
2019-02-20 16:18:58 +0800

    delete some obj flie

--- .at88sc.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/mnt/hgfs/share/at88sc/at88sc.ko := arm-linux-gnueabihf-ld -EL -r  -T /opt/uwork/sina33-v2/vendor/softwinner/linux-3.4/scripts/module-common.lds --build-id  -o /mnt/hgfs/share/at88sc/at88sc.ko /mnt/hgfs/share/at88sc/at88sc.o /mnt/hgfs/share/at88sc/at88sc.mod.o
